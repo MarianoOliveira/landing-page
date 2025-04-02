41417dd5ce5910d49a02c92986cb6e63863ddf61
Mariano Oliveira <m.oliveira3000@hotmail.com>
2025-04-02 18:20:02 -0300

    fix: remove react-router to fix build error

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "./components/ui/toaster";
 import { Toaster as Sonner } from "./components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 
 const queryClient = new QueryClient();
@@ -10,12 +9,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <Toaster />
     <Sonner />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="*" element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
+    <Landing />
   </QueryClientProvider>
 );
 
