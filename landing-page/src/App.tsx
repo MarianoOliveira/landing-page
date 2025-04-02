0cba1e533f33004b24e30cc16a1fffe9b940c59e
Mariano Oliveira <m.oliveira3000@hotmail.com>
2025-04-02 16:40:03 -0300

    tentar projeto dentro de projeto vecel 4

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -3,29 +3,19 @@ import { Toaster as Sonner } from "./components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 
 const queryClient = new QueryClient();
 
-// Wrapper component for TooltipProvider
-const TooltipWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <TooltipPrimitive.Provider delayDuration={0}>
-    {children}
-  </TooltipPrimitive.Provider>
-);
-
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipWrapper>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter basename="/landing-page">
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="*" element={<Landing />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipWrapper>
+    <Toaster />
+    <Sonner />
+    <BrowserRouter basename="/landing-page">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="*" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
   </QueryClientProvider>
 );
 
