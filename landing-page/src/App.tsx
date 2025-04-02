0a98055c1e645fb5e7f4f1f485d965bdc0482e60
Mariano Oliveira <m.oliveira3000@hotmail.com>
2025-04-02 16:33:35 -0300

    tentar projeto dentro de projeto vecel 2

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from "./components/ui/toaster";
 import { Toaster as Sonner } from "./components/ui/sonner";
-import { TooltipProvider } from "./components/ui/tooltip";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
@@ -9,7 +9,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
+    <TooltipPrimitive.Provider>
       <Toaster />
       <Sonner />
       <BrowserRouter basename="/landing-page">
@@ -18,7 +18,7 @@ const App = () => (
           <Route path="*" element={<Landing />} />
         </Routes>
       </BrowserRouter>
-    </TooltipProvider>
+    </TooltipPrimitive.Provider>
   </QueryClientProvider>
 );
 
